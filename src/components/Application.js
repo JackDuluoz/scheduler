e45d74bbb97aeb3a3cdb02f6b4ef70e1024b2b39
Michael Green <greenmr95@hotmail.com>
2023-01-12 14:12:19 -0800

    completed day 2 material

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
+import DayList from "./DayList";
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
 
 export default function Application(props) {
+
+  const [day, setDay] = useState("Monday");
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -12,7 +34,13 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            day={day}
+            setDay={setDay}
+          />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
@@ -22,6 +50,7 @@ export default function Application(props) {
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
       </section>
-    </main>
+    </main>    
   );
+
 }
---
+++ src/components/DayListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
+  const remainingSpots = function (spots) {
+    if (spots === 0) {
+      return "no spots remaining"
+    } else if (spots === 1) {
+      return "1 spot remaining"
+    } else {
+      return spots + " spots remaining"
+    }
+  }
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{remainingSpots(props.spots)}</h3>
+    </li>
+  );
+  
+}
